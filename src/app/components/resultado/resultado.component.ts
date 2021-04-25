2ee13424798b67150460a27ebc00adb8004f6d76
Alejahndro <alejahndro02@gmail.com>
2021-04-25 08:37:48 -0500

    Se declararon las variables de encabezado y textoResultado, también se creo el metodo de getResultado()

--- src/app/components/resultado/resultado.component.ts
+++ src/app/components/resultado/resultado.component.ts
@@ -8,11 +8,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultadoComponent implements OnInit {
   bmi: number;
+  encabezado: string;
+  textoResultado: string;
   constructor(private route:ActivatedRoute ) {
+    this.encabezado = '';
+    this.textoResultado ='';
     this.bmi = +route.snapshot.paramMap.get('valor')!;
    }
 
   ngOnInit(): void {
+    this.getResultado();
+  }
+  getResultado(){
+    if(this.bmi >=25 ){
+      this.encabezado = 'Exceso de Peso'
+      this.textoResultado = 'Tienes un peso coorporal superiror al normal.'
+    }else if(this.bmi >=18.5 ){
+      this.encabezado = 'Normal'
+      this.textoResultado = 'Tu peso coorporal es normal.'
+    }else{
+      this.encabezado = 'Bajo'
+      this.textoResultado = 'Tu peso coorporal es más bajo de lo normal.'
+
+    }
   }
 
 }
